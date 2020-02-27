15a735c6b0b39adbe3fbb69fcbea4dfff502b796
Diego Rocha <dlmrocha87@gmail.com>
2020-02-27 18:46:31 -0300

    Added ctable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 
 const ALPHABET: [char; 26] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
@@ -13,34 +14,38 @@ struct Enigma {
 
     // current rotor in action
     rot_base: usize,
+    ctable: BTreeMap<char, char>,
 }
 // A:10 -> 12 -> 17:R)|
 impl Enigma {
-    fn spin_rot(&mut self) {
+    fn inc_rot(rot: &mut [usize; 26]) {
         for index in 0..26 {
-            match self.rot_base {
-                0 => {
-                    self.rot1[index] += 1;
-                    if self.rot1[index] == 26 {
-                        self.rot1[index] = 0;
-                    }
-                }
-                1 => {
-                    self.rot2[index] += 1;
-                    if self.rot2[index] == 26 {
-                        self.rot2[index] = 0;
-                    }
-                }
-                2 => {
-                    self.rot3[index] += 1;
-                    if self.rot3[index] == 26 {
-                        self.rot3[index] = 0;
-                    }
-                }
-		_ => unimplemented!(),
+            rot[index] += 1;
+            if rot[index] == 26 {
+                rot[index] = 0;
             }
         }
+    }
 
+    fn set_rot(rot: &mut [usize; 26], count: usize) {
+        let mut count = count;
+        for index in 0..26 {
+            rot[index] = count;
+            if count >= 25 {
+                count = 0;
+            } else {
+                count += 1;
+            }
+        }
+    }
+
+    fn spin_rot(&mut self) {
+        match self.rot_base {
+            0 => Enigma::inc_rot(&mut self.rot1),
+            1 => Enigma::inc_rot(&mut self.rot2),
+            2 => Enigma::inc_rot(&mut self.rot3),
+            _ => unimplemented!(),
+        }
         if self.rot_base >= 2 {
             self.rot_base = 0;
         } else {
@@ -49,76 +54,99 @@ impl Enigma {
     }
 
     fn crypt_char(&mut self, ch: char) -> char {
-        let ch = ALPHABET[self.rot3[self.rot2[self.rot1[ch as usize - 65]]]];
-	self.spin_rot();
-	ch
+        if ch != ' ' {
+            let ch = ALPHABET[self.rot3[self.rot2[self.rot1[ch as usize - 65]]]];
+            self.spin_rot();
+            *self.ctable.get(&ch).unwrap_or(&ch)
+        } else {
+            ch
+        }
     }
 
+    // FIXME: remove unwraps
     fn decipher_char(&mut self, ch: char) -> char {
-	let p1 = self.rot3.iter().position(|x| *x == (ch as usize - 65)).unwrap();
-	let p2 = self.rot2.iter().position(|x| *x == p1).unwrap();
-	let p3 = self.rot1.iter().position(|x| *x == p2).unwrap();
-	// println!("p1: {}", p3);
-	let ch = ALPHABET[p3];
-	
-	self.spin_rot();
-	ch
+        if ch != ' ' {
+            let p1 = self
+                .rot3
+                .iter()
+                .position(|x| *x == (ch as usize - 65))
+                .unwrap();
+            let p2 = self.rot2.iter().position(|x| *x == p1).unwrap();
+            let p3 = self.rot1.iter().position(|x| *x == p2).unwrap();
+            let ch = ALPHABET[p3];
+            self.spin_rot();
+            *self.ctable.get(&ch).unwrap_or(&ch)
+        } else {
+            ch
+        }
     }
 
-    fn new(mut p1: usize, mut p2: usize, mut p3: usize) -> Self {
-        let mut r1 = [0; 26];
-        for index in 0..26 {
-            r1[index] = p1;
-            if p1 >= 25 {
-                p1 = 0;
-            } else {
-                p1 += 1;
-            }
-        }
+    fn new(p1: usize, p2: usize, p3: usize, ctable: BTreeMap<char, char>) -> Self {
+        let mut enigma = Self {
+            rot1: [0; 26],
+            rot2: [0; 26],
+            rot3: [0; 26],
 
-        let mut r2 = [0; 26];
-        for index in 0..26 {
-            r2[index] = p2;
-            if p2 >= 25 {
-                p2 = 0;
-            } else {
-                p2 += 1;
-            }
-        }
-
-        let mut r3 = [0; 26];
-        for index in 0..26 {
-            r3[index] = p3;
-            if p3 >= 25 {
-                p3 = 0;
-            } else {
-                p3 += 1;
-            }
-        }
-        Self {
-            rot1: r1,
-            rot2: r2,
-            rot3: r3,
             rot_base: 0,
-        }
+            ctable,
+        };
+        Enigma::set_rot(&mut enigma.rot1, p1);
+        Enigma::set_rot(&mut enigma.rot2, p2);
+        Enigma::set_rot(&mut enigma.rot3, p3);
+        enigma
     }
 }
 
 fn main() {
-    
-    let mut enigma = Enigma::new(5,12,1);
-    // println!("{:?}\n{:?}\n{:?}\n", enigma.rot1, enigma.rot2, enigma.rot3);
-    let mut res: String = String::new();
-    let msg = String::from("BASILIAEDIEGOPRASEMPREJUNTOS");
-    for ch in msg.as_bytes() {
-	 res.push(enigma.crypt_char(*ch as char));
+    let msg = String::from("ITS MY LIFE IS NOW OR NEVER");
+
+    let mut ctable: BTreeMap<char, char> = BTreeMap::new();
+    let t = String::from("ABIJHUTKZWSVGLCFDE");
+    let mut citer = t.chars();
+    loop {
+        let k = citer.next();
+        let v = citer.next();
+        if k.is_none() || v.is_none() {
+            break;
+        }
+        let k = k.unwrap();
+        let v = v.unwrap();
+        ctable.insert(k, v);
+        ctable.insert(v, k);
     }
+
+    let mut res: String = String::new();
+    let mut enigma = Enigma::new(0, 25, 0, ctable);
+
+    for ch in msg.chars() {
+        let ch = enigma.crypt_char(ch);
+        res.push(ch);
+    }
+
     println!("{}", res);
 
-
-    let mut enigma = Enigma::new(5,12,1);
-    let msg = String::from("TTMDHFY");
-    for ch in msg.as_bytes() {
-	println!("{} -> {}", *ch as char, enigma.decipher_char(*ch as char));
+    let mut res: String = String::new();
+    let mut ctable: BTreeMap<char, char> = BTreeMap::new();
+    let t = String::from("AGBIJHUTLKZWSV");
+    let mut citer = t.chars();
+    loop {
+        let k = citer.next();
+        let v = citer.next();
+        if k.is_none() || v.is_none() {
+            break;
+        }
+        let k = k.unwrap();
+        let v = v.unwrap();
+        ctable.insert(k, v);
+        ctable.insert(v, k);
     }
+
+    let msg = String::from("AZPWGYE D DCGDS UXN DNWADR VIDLGO");
+    let mut enigma = Enigma::new(5, 12, 1, ctable);
+
+    for ch in msg.chars() {
+        let ch = enigma.decipher_char(ch);
+        res.push(ch);
+    }
+    println!("{}", res);
 }
